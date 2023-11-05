512172f12dc6eb9bc721607e375dcf3cf620853b
SFERAS <rthyssen73@gmail.com>
2023-11-05 00:03:45 -0300

    componenet button spread created

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App'
-// import { createVuestic } from 'vuestic-ui'
-// import 'vuestic-ui/css'
 import components from "@/components/UI"
 
-console.log(components)
+const app = createApp(App)
 
-createApp(App).mount('#app')
+components.forEach(component =>{
+	app.component(component.name, component)
+})
+
+app.mount('#app')
